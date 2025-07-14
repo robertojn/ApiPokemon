55be482f23008d0c942480cfd7d24b47a8178edf
robertojn <127152647+robertojn@users.noreply.github.com>
2025-07-14 13:29:23 -0300

    refactor: mover lógica de mapeamento para PokeApiMapper

---
+++ PokemonApi.Infrastructure/Mappers/PokeApiMapper.cs
@@ -0,0 +1,25 @@
+﻿using PokemonApi.Domain.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonApi.Infrastructure.Repositories;
+
+
+namespace PokemonApi.Infrastructure.Mappers
+{
+    public static class PokeApiMapper
+    {
+        public static PokemonDto ToPokemonDto(PokeApiResponse response)
+        {
+            return new PokemonDto
+            {
+                Name = response.Name,
+                Height = response.Height,
+                Weight = response.Weight,
+                Types = response.Types.Select(t => t.Type.Name).ToList()
+            };
+        }
+    }
+}
